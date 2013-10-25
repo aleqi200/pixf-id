f73f06d9f39888281878e4e8ecd3252eb19c3b52
Alexandre Campelo <aleqi200@gmail.com>
2013-10-25 14:09:30 +0200

    tests added

--- test/controllers/sequence_controller_controller_test.rb
+++ test/controllers/sequence_controller_controller_test.rb
@@ -5,5 +5,14 @@ class SequenceControllerControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+  test "response should differ" do
+    get :index
+    response_one = response.body
+    assert_response :success
 
+    get :index
+    response_two = response.body
+    assert_response :success
+    assert (not response_two.eql? response_one)
+  end
 end
